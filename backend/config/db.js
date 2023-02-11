ce2bb3002bb9a881400aeb6301aa0dc98043fbdf
Tadeo Alvarez <tadeo.regalado.alvarez@gmail.com>
2023-02-10 21:46:14 -0600

    Connecting MySQL database to express
    We connect the MySQL database to express using sequelize

---
+++ backend/config/db.js
@@ -0,0 +1,11 @@
+import { Sequelize } from "sequelize"; //importing sequelize to manipulate
+import dotenv from 'dotenv/config'; //telling sequelize to read dotenv
+
+//creating an instance of the db connection
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    port: '3306'
+});
+
+export default db;
--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,23 @@
 //libraries
 import express, { json } from "express";
+import dotenv from 'dotenv';
+
+//files
+import db from "./config/db.js";
 
 //variables
 const app = express(); //instancing express
+dotenv.config(); //using enviroment variables
 app.use(json()); //using json 
 const port = process.env.PORT || 3000; //indicating port
+
+//db connection
+try {
+    await db.authenticate();
+    console.log('Connection to db has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
 
 //entry point
 app.get('/', (req, res) => {
