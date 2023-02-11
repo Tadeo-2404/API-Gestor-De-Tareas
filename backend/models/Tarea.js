c670c875c3c8099751de1712a5b2402027718850
Tadeo Alvarez <tadeo.regalado.alvarez@gmail.com>
2023-02-10 22:10:31 -0600

    Creating the Tarea Model
    Creating the Tarea Model for the db

---
+++ backend/models/Tarea.js
@@ -0,0 +1,38 @@
+import { DataTypes } from 'sequelize';
+import db from '../config/db.js';
+
+export const Tarea = db.define('Tarea', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    titulo: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    descripcion: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    completado: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+    },
+    fecha_de_entrega: {
+        type: DataTypes.DATE,
+        allowNull: false,
+    },
+    comentarios: {
+        type: DataTypes.STRING,
+    },
+    responsable: {
+        type: DataTypes.INTEGER,
+        references: 'usuario',
+        referencesKey: 'id',
+    }
+}, {
+    timestamps: true,
+    updatedAt: false,
+    createdAt: false,
+});
