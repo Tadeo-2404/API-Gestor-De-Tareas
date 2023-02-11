d7be92b1aa887e2ad0b5867481f4f99d8aaca531
Tadeo Alvarez <tadeo.regalado.alvarez@gmail.com>
2023-02-11 12:22:32 -0600

    EditarTarea controller
    Creating the controller for editing an item

--- backend/controllers/TareaController.js
+++ backend/controllers/TareaController.js
@@ -8,9 +8,7 @@ const obtenerTareas = (req, res) => {
 //create task
 const agregarTarea = async (req, res) => {
     const tarea = new Tarea(req.body); //reading user input
-    const responsable = localStorage.getItem("responsable"); //get value from localstorage
-    tarea.responsable = responsable; //assing value to variable
-    const {titulo , descripcion, fecha_de_entrega} = tarea; //destructing to validate
+    const {titulo , descripcion, fecha_de_entrega, responsable} = tarea; //destructing to validate
 
     //validation
     if(!titulo || !descripcion || !fecha_de_entrega) {
@@ -34,10 +32,11 @@ const agregarTarea = async (req, res) => {
     }
 }
 
+//getting a task
 const obtenerTarea = async (req, res) => {
     const {id} = req.params; //read item id from url
-    const responsable = localStorage.getItem("responsable"); //get value from localstorage
-    
+    const {responsable} = req.body; //get value from localstorage
+
     const tarea = await Tarea.findOne({
         where: {
               id: id, //where the id is the same
@@ -67,11 +66,48 @@ const obtenerTarea = async (req, res) => {
     res.json({tarea});
 }
 
-const editarTarea = (req, res) => {
-    const {id} = req.params;
-    res.json({msg: `editando tarea: ${id}`});
+//editing a task
+const editarTarea = async (req, res) => {
+    const {id} = req.params; //read item id from url
+    const {titulo, descripcion, fecha_de_entrega, completado, comentarios, responsable} = req.body; //get value from user input
+    
+    const tarea = await Tarea.findOne({
+        where: {
+              id: id, //where the id is the same
+              responsable: responsable //so is the name
+          }
+    }); //search in db
+
+    //validation tarea not found
+    if(!tarea) {
+        const error = new Error("item not found");
+        res.status(400).json({msg: error.message});
+    }
+
+    //validation responsable not found
+    if(!responsable) {
+        const error = new Error("responsanble missing");
+        res.status(400).json({msg: error.message});
+        return;
+    }
+
+    //check if any changes were made, if so then update the values 
+    titulo ? tarea.titulo = titulo : tarea.titulo = tarea.titulo
+    descripcion ? tarea.descripcion = descripcion : tarea.descripcion = tarea.descripcion
+    completado ? tarea.completado = completado : tarea.completado = tarea.completado
+    fecha_de_entrega ? tarea.fecha_de_entrega = fecha_de_entrega : tarea.fecha_de_entrega = tarea.fecha_de_entrega
+    comentarios ? tarea.comentarios = comentarios : tarea.comentarios = tarea.comentarios
+
+    try {
+        const actualizado = await tarea.save(); //saving the changes
+        res.json(actualizado);
+    } catch (e) {
+        const error = new Error("item could not be updated");
+        res.status(400).json({msg: error.message});
+    }
 }
 
+//deleting a task
 const eliminarTarea = (req, res) => {
     const {id} = req.params;
     res.json({msg: `borrando tarea: ${id}`});
