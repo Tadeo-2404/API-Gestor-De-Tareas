ae4b992e36bcaf6f8922bfa818ecf926d8b3f48c
Tadeo Alvarez <tadeo.regalado.alvarez@gmail.com>
2023-02-11 21:53:19 -0600

    Creating the user and dynamic navbar
    creating a user and making a dynamic navbar

---
+++ frontend/src/components/NavBarAuth.jsx
@@ -0,0 +1,11 @@
+const NavBarAuth = () => {
+  return (
+    <div className="bg-yellow-500 w-full p-8 flex justify-center font-bold text-white uppercase text-2xl">
+        <div className='w-full text-center'>
+            <h1>gestor de tareas</h1>
+        </div>
+    </div>
+  )
+}
+
+export default NavBarAuth
--- frontend/src/components/layout/TemplateLayout.jsx
+++ frontend/src/components/layout/TemplateLayout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import NavBar from '../NavBar';
+import NavBarAuth from '../NavBarAuth';
 const TemplateLayout = () => {
+  const user = localStorage.getItem('responsable');
   return (
     <div>
-        <NavBar />
+        {user ? <NavBar /> : <NavBarAuth />}
         <div className='p-10 h-screen flex flex-col justify-center items-center'>
          <Outlet />
         </div>
